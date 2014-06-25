879f39b31d876926e988dd44150001d0f40d0f91
Marek Hlava <headsvk@gmail.com>
2014-06-25 15:39:49 +0200

    fixed camera preview

--- app/src/main/java/datagridapp/packagereader/CaptureActivity.java
+++ app/src/main/java/datagridapp/packagereader/CaptureActivity.java
@@ -22,14 +22,15 @@ import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.KeyEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import com.google.zxing.Result;
 import com.google.zxing.client.result.ParsedResult;
 import com.google.zxing.client.result.ParsedResultType;
@@ -44,143 +45,142 @@ import java.io.IOException;
  */
 public final class CaptureActivity extends Activity implements SurfaceHolder.Callback {
 
-  private static final String TAG = CaptureActivity.class.getSimpleName();
+    private static final String TAG = CaptureActivity.class.getSimpleName();
 
-  private boolean hasSurface;
-  private SurfaceHolder holderWithCallback;
-  private Camera camera;
-  private DecodeRunnable decodeRunnable;
-  private Result result;
+    private boolean hasSurface;
+    private SurfaceHolder holderWithCallback;
+    private Camera camera;
+    private DecodeRunnable decodeRunnable;
+    private Result result;
 
-  @Override
-  public void onCreate(Bundle icicle) {
-    super.onCreate(icicle);
-    Window window = getWindow();
-    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-    setContentView(R.layout.capture);
-  }
-
-  @Override
-  public synchronized void onResume() {
-    super.onResume();
-    SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
-    SurfaceHolder surfaceHolder = surfaceView.getHolder();
-    if (surfaceHolder == null) {
-      throw new IllegalStateException("No SurfaceHolder?");
-    }
-    if (hasSurface) {
-      initCamera(surfaceHolder);
-    } else {
-      surfaceHolder.addCallback(this);
-      holderWithCallback = surfaceHolder;
-    }
-  }
-
-  @Override
-  public synchronized void onPause() {
-    result = null;
-    if (decodeRunnable != null) {
-      decodeRunnable.stop();
-      decodeRunnable = null;
-    }
-    if (camera != null) {
-      camera.stopPreview();
-      camera.release();
-      camera = null;
-    }
-    if (holderWithCallback != null) {
-      holderWithCallback.removeCallback(this);
-      holderWithCallback = null;
-    }
-    super.onPause();
-  }
-
-  @Override
-  public synchronized void surfaceCreated(SurfaceHolder holder) {
-    Log.i(TAG, "Surface created");
-    holderWithCallback = null;
-    if (!hasSurface) {
-      hasSurface = true;
-      initCamera(holder);
-    }
-  }
-
-  @Override
-  public synchronized void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-    // do nothing
-  }
-
-  @Override
-  public synchronized void surfaceDestroyed(SurfaceHolder holder) {
-    Log.i(TAG, "Surface destroyed");
-    holderWithCallback = null;
-    hasSurface = false;
-  }
-
-  @Override
-  public boolean onKeyDown(int keyCode, KeyEvent event) {
-    if (result != null) {
-      switch (keyCode) {
-        case KeyEvent.KEYCODE_DPAD_CENTER:
-          handleResult(result);
-          return true;
-        case KeyEvent.KEYCODE_BACK:
-          reset();
-          return true;
-      }
-    }
-    return super.onKeyDown(keyCode, event);
-  }
-
-  private void initCamera(SurfaceHolder holder) {
-    if (camera != null) {
-      throw new IllegalStateException("Camera not null on initialization");
-    }
-    camera = Camera.open();
-    if (camera == null) {
-      throw new IllegalStateException("Camera is null");
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.capture);
     }
 
-    CameraConfigurationManager.configure(camera);
-
-    try {
-      camera.setPreviewDisplay(holder);
-      camera.startPreview();
-    } catch (IOException e) {
-      Log.e(TAG, "Cannot start prevew", e);
+    @Override
+    public synchronized void onResume() {
+        super.onResume();
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        if (surfaceHolder == null) {
+            throw new IllegalStateException("No SurfaceHolder?");
+        }
+        if (hasSurface) {
+            initCamera(surfaceHolder);
+        } else {
+            surfaceHolder.addCallback(this);
+            holderWithCallback = surfaceHolder;
+        }
     }
 
-    decodeRunnable = new DecodeRunnable(this, camera);
-    new Thread(decodeRunnable).start();
-    reset();
-  }
-
-  void setResult(Result result) {
-    TextView statusView = (TextView) findViewById(R.id.status_view);
-    String text = result.getText();
-    statusView.setText(text);
-    statusView.setTextSize(TypedValue.COMPLEX_UNIT_SP, Math.max(14, 72 - text.length() / 2));
-    statusView.setVisibility(View.VISIBLE);
-    this.result = result;
-  }
-
-  private void handleResult(Result result) {
-    ParsedResult parsed = ResultParser.parseResult(result);
-    Intent intent;
-    if (parsed.getType() == ParsedResultType.URI) {
-      intent = new Intent(Intent.ACTION_VIEW, Uri.parse(((URIParsedResult) parsed).getURI()));
-    } else {
-      intent = new Intent(Intent.ACTION_WEB_SEARCH);
-      intent.putExtra("query", ((TextParsedResult) parsed).getText());
+    @Override
+    public synchronized void onPause() {
+        result = null;
+        if (decodeRunnable != null) {
+            decodeRunnable.stop();
+            decodeRunnable = null;
+        }
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+        if (holderWithCallback != null) {
+            holderWithCallback.removeCallback(this);
+            holderWithCallback = null;
+        }
+        super.onPause();
     }
-    startActivity(intent);
-  }
 
-  private void reset() {
-    TextView statusView = (TextView) findViewById(R.id.status_view);
-    statusView.setVisibility(View.GONE);
-    result = null;
-    decodeRunnable.startScanning();
-  }
+    @Override
+    public synchronized void surfaceCreated(SurfaceHolder holder) {
+        Log.i(TAG, "Surface created");
+        holderWithCallback = null;
+        if (!hasSurface) {
+            hasSurface = true;
+            initCamera(holder);
+        }
+    }
+
+    @Override
+    public synchronized void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // do nothing
+    }
+
+    @Override
+    public synchronized void surfaceDestroyed(SurfaceHolder holder) {
+        Log.i(TAG, "Surface destroyed");
+        holderWithCallback = null;
+        hasSurface = false;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (result != null) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_DPAD_CENTER:
+                    handleResult(result);
+                    return true;
+                case KeyEvent.KEYCODE_BACK:
+                    reset();
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void initCamera(SurfaceHolder holder) {
+        if (camera != null) {
+            throw new IllegalStateException("Camera not null on initialization");
+        }
+        camera = Camera.open();
+        if (camera == null) {
+            throw new IllegalStateException("Camera is null");
+        }
+
+        CameraConfigurationManager.configure(camera);
+
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            Log.e(TAG, "Cannot start prevew", e);
+        }
+
+        decodeRunnable = new DecodeRunnable(this, camera);
+        new Thread(decodeRunnable).start();
+        reset();
+    }
+
+    void setResult(Result result) {
+        ListView statusView = (ListView) findViewById(R.id.status_view);
+        String[] textArr = result.getText().split(";");
+        statusView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, textArr));
+//        statusView.setTextSize(TypedValue.COMPLEX_UNIT_SP, Math.max(14, 72 - text.length() / 2));
+        statusView.setVisibility(View.VISIBLE);
+        this.result = result;
+    }
+
+    private void handleResult(Result result) {
+        ParsedResult parsed = ResultParser.parseResult(result);
+        Intent intent;
+        if (parsed.getType() == ParsedResultType.URI) {
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(((URIParsedResult) parsed).getURI()));
+        } else {
+            intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            intent.putExtra("query", ((TextParsedResult) parsed).getText());
+        }
+        startActivity(intent);
+    }
+
+    private void reset() {
+        findViewById(R.id.status_view).setVisibility(View.GONE);
+        result = null;
+        decodeRunnable.startScanning();
+    }
 
 }
--- app/src/main/java/datagridapp/packagereader/MainActivity.java
+++ app/src/main/java/datagridapp/packagereader/MainActivity.java
@@ -29,21 +29,22 @@ public class MainActivity extends Activity {
     private void processVoiceAction() {
         Intent intent = new Intent("com.google.zxing.client.android.SCAN");
         intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-        startActivityForResult(intent, 0);
+        startActivity(intent);
+//        startActivityForResult(intent, 0);
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
-                String contents = intent.getStringExtra("SCAN_RESULT");
-                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-                // Handle successful scan
-                Intent result = new Intent(this,ResultActivity.class);
-                result.putExtra("contents", contents);
-                startActivity(result);
-            } else if (resultCode == RESULT_CANCELED) {
-                // Handle cancel
-            }
-        }
-    }
+//    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+//        if (requestCode == 0) {
+//            if (resultCode == RESULT_OK) {
+//                String contents = intent.getStringExtra("SCAN_RESULT");
+//                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+//                // Handle successful scan
+//                Intent result = new Intent(this,ResultActivity.class);
+//                result.putExtra("contents", contents);
+//                startActivity(result);
+//            } else if (resultCode == RESULT_CANCELED) {
+//                // Handle cancel
+//            }
+//        }
+//    }
 }
